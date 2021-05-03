ddff463889f7bf08d8b59f23d19ceede506259aa
nerrevar <nerrevar12@yandex.ru>
2021-05-03 15:01:15 +0300

    Cart button

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,105 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <router-view />
+  <div class="cart" v-bind="$attrs">
+    <div class="cart__image">
+      <img src="@/assets/img/cart.png" alt="&#128722;" /><!--replace with unicode image-->
+    </div>
+    <div class="cart__border"></div>
+    <div
+      class="cart__count"
+      v-if="getCartCount !== 0"
+    >
+      <span>{{ getCartCount }}</span>
+    </div>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default defineComponent ({
+  name: 'Layout',
+  computed: {
+    getCartCount () {
+      return 1
     }
-  }
-}
+  },
+})
+</script>
+
+<style lang="sass" scoped>
+$cart-size: 5rem
+
+@keyframes pulse-border
+  0%
+    width: calc(#{$cart-size} - 0.5rem)
+    height: calc(#{$cart-size} - 0.5rem)
+    top: 0.25rem
+    left: 0.25rem
+  50%
+    width: $cart-size
+    height: $cart-size
+    top: 0
+    left: 0
+  100%
+    width: calc(#{$cart-size} - 0.5rem)
+    height: calc(#{$cart-size} - 0.5rem)
+    top: 0.25rem
+    left: 0.25rem
+
+.cart
+  position: absolute
+  right: 3rem
+  bottom: 3rem
+  width: $cart-size
+  height: $cart-size
+
+  &__border
+    position: absolute
+    top: 0.25rem
+    left: 0.25rem
+    width: calc(#{$cart-size} - 0.5rem)
+    height: calc(#{$cart-size} - 0.5rem)
+    z-index: 101
+
+    background-color: #F57F17
+    border-radius: 50%
+
+    animation-name: pulse-border
+    animation-duration: 2s
+    animation-fill-mode: forwards
+    animation-timing-function: linear
+    animation-iteration-count: infinite
+
+  &__image
+    position: absolute
+    top: 0.5rem
+    left: 0.5rem
+    width: calc(#{$cart-size} - 1rem)
+    height: calc(#{$cart-size} - 1rem)
+    z-index: 102
+
+    border-radius: 50%
+    background-color: yellow
+
+    & > img
+      margin: 15%
+      width: 70%
+      height: 70%
+
+      text-align: center
+      font-size: calc(#{$cart-size} - 2rem)
+
+  &__count
+    position: absolute
+    left: calc(#{$cart-size} - 1.5rem)
+    top: calc(#{$cart-size} - 1.5rem)
+    width: 1.2rem
+    height: 1.2rem
+    z-index: 103
+
+    border-radius: 50%
+    background-color: red
+    color: wheat
+    text-align: center
 </style>
